508c09120f6a7df50418a5dd3e15c460c7c672cf
Tanasit Auewongaree <006006@cdg.co.th>
2021-03-09 17:12:36 +0700

    Initial Commit

---
+++ SwiftUI-PageAwesome-2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-PageAwesome-2
+//
+//  Created by BillY-MAC on 9/3/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-PageAwesome-2/SwiftUI_PageAwesome_2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PageAwesome_2App.swift
+//  SwiftUI-PageAwesome-2
+//
+//  Created by BillY-MAC on 9/3/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PageAwesome_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
