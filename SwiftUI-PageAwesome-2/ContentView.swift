99bcdde2c026618f20a435f0e97c67d0b795609b
Tanasit Auewongaree <006006@cdg.co.th>
2021-03-09 17:30:15 +0700

    page awesome2

--- SwiftUI-PageAwesome-2/ContentView.swift
+++ SwiftUI-PageAwesome-2/ContentView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("business_background")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack {
+                Text("Metro Center Station")
+                    .font(.system(size: 36, weight: .bold,design: .default))
+                    .foregroundColor(Color.white)
+                    .shadow(color: Color.gray,radius: 3,x: 3,y: 3 )
+                    .padding(.top, 20)
+                
+                Spacer()
+                
+                Text("GISC, Co,LTD. CDG Group.")
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundColor(Color.white)
+                    .shadow(color: Color.black,radius: 3,x: 3,y: 3 )
+                    .padding(.bottom, 20)
+            }
+            
+        }
     }
 }
 
